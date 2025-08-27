ab46a16d8bd6ba037ebaf6eba7e19fdeb046143b
tamamart <tamamart@student.42madrid.com>
2025-08-27 16:38:17 +0200

    ft_strrchr añadido

---
+++ ft_strrchr.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tamamart <tamamart@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/27 16:19:51 by tamamart          #+#    #+#             */
+/*   Updated: 2025/08/27 16:37:43 by tamamart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = ft_strlen(s);
+	while (i + 1 > 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		if (i == 0)
+			break ;
+		i--;
+	}
+	return (0);
+}
+
+/* #include <stdio.h>
+#include <string.h>
+int	main(void)
+{
+	const char	*s = "Hola mundo insensato!";
+	int			c;
+	char		*result1;
+	char		*result2;
+	c = 'a';
+	result1 = ft_strrchr(s, c);
+	result2 = strrchr(s, c);
+	printf("ft_strrchr: %s\n", result1);
+	printf("   strrchr: %s\n", result2);
+	c = 'z';
+	result1 = ft_strrchr(s, c);
+	result2 = strrchr(s, c);
+	printf("ft_strrchr: %s\n", result1);
+	printf("   strrchr: %s\n", result2);
+	c = '\0';
+	result1 = ft_strrchr(s, c);
+	result2 = strrchr(s, c);
+	printf("ft_strrchr: %s\n", result1);
+	printf("   strrchr: %s\n", result2);
+	return (0);
+} */
